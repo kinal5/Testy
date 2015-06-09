7e4bb6662e6c4a687c5cc8d31a875150a5f29fe3
kinal5 <monika.cinal@gmail.com>
2015-06-10 01:15:17 +0200

    Dodanie interfejsu z przypadkami testowymi
    Dodanie interfejsu z przypadkami testowymi

---
+++ Zadanie/Zadanie/Interface1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie
+{
+    interface Interface1
+    {
+
+        /// <summary>
+        /// Gets solutions of the problem
+        /// </summary>
+        /// <param name="lowestMultiple">the number with is the lowest multiple</param>
+        /// <usecase result="2">Calc(2)</usecase>
+        /// <usecase result="6">Calc(3)</usecase>
+        /// <usecase result="60">Calc(5)</usecase>
+        /// <usecase result="420">Calc(7)</usecase>
+        /// <usecase result="2520">Calc(10)</usecase>
+        /// <usecase result="232792560">Calc(20)</usecase>
+        /// <returns>solution</returns>
+        static int Calc(int n);
+    }
+}
+
--- Zadanie/Zadanie/Program.cs
+++ Zadanie/Zadanie/Program.cs
@@ -9,7 +9,7 @@ namespace Zadanie
 
     class Program
     {
-      
+        
         static void Main(string[] args)
         {
             
